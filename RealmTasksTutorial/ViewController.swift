b31be8830b0901353963c9f28fe24da9bee103f7
Victor Lee <myleevictor@gmail.com>
2017-05-22 17:42:06 +0800

    added data model for TaskList and Task objects

--- RealmTasksTutorial/ViewController.swift
+++ RealmTasksTutorial/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import RealmSwift
+
+final class TaskList: Object {
+    
+    dynamic var text = ""
+    dynamic var id = ""
+    let items = List<Task>()
+    
+    override static func primaryKey() -> String {
+        return "id"
+    }
+}
+
+final class Task: Object {
+    dynamic var text = ""
+    dynamic var completed = false
+}
+
 
 class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
+    
     
 
 
